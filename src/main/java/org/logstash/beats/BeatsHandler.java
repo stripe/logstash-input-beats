5797dfd794478914f03c5f3a8cc5e0f28b523843
Hans Nielsen <hans@stripe.com>
2017-04-03 14:00:12 -0700

    Fix plugin crashing non-client-cert TLS

--- src/main/java/org/logstash/beats/BeatsHandler.java
+++ src/main/java/org/logstash/beats/BeatsHandler.java
@@ -14,7 +14,6 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.concurrent.atomic.AtomicBoolean;
 import javax.net.ssl.SSLPeerUnverifiedException;
-import javax.security.cert.X509Certificate;
 import javax.security.auth.x500.X500Principal;
 
 public class BeatsHandler extends SimpleChannelInboundHandler<Batch> {
@@ -98,7 +97,7 @@ public class BeatsHandler extends SimpleChannelInboundHandler<Batch> {
             } else if(e.state() == IdleState.READER_IDLE) {
                 clientTimeout();
             }
-        } else if(event instanceof SslHandshakeCompletionEvent) {
+        } else if(sslHandler != null && event instanceof SslHandshakeCompletionEvent) {
             try {
                 peerDn = sslHandler.engine().getSession().getPeerPrincipal().toString();
                 logger.info("Got peer DN '" + peerDn + "'");
