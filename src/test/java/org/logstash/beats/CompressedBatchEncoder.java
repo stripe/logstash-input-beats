beab1f57d290b84fa86ff8651142583f0c13ea45
Hans Nielsen <hans@stripe.com>
2017-04-05 11:40:28 -0700

    Upgrade to log4j2 to be compatible with logstash 5.2.0

--- src/test/java/org/logstash/beats/CompressedBatchEncoder.java
+++ src/test/java/org/logstash/beats/CompressedBatchEncoder.java
@@ -5,7 +5,6 @@ import io.netty.buffer.ByteBufOutputStream;
 import io.netty.channel.ChannelHandlerContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import java.io.IOException;
 import java.util.zip.Deflater;
 import java.util.zip.DeflaterOutputStream;
